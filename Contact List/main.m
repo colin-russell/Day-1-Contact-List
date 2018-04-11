5a8dd307318e8cdd5836eebe008423557bd921e3
Colin Russell <colinrussell101@gmail.com>
2018-04-10 21:04:48 -0400

    finished working on the new contact menu item

--- Contact List/main.m
+++ Contact List/main.m
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "InputCollector.h"
 #import "ContactList.h"
+#import "Contact.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -21,12 +22,23 @@ int main(int argc, const char * argv[]) {
         while (gameOn == YES) {
             
             NSString *mainMenu = [inputCollector inputForPrompt:@"What would you like do next? new - Create a new contact | list - List all contacts | quit - Exit Application "];
-            
+           
+            // cycle through the menu items
             if ([mainMenu isEqualToString:@"new"]) {
-                NSLog(@"NEW");
+                NSString *inputName = [inputCollector inputForPrompt:@"Enter a name for the new contact"];
+                NSString *inputEmail = [inputCollector inputForPrompt:@"Now enter an email address"];
+                
+                Contact *newContact = [[Contact alloc] init];
+                newContact.name = inputName;
+                newContact.email = inputEmail;
+                
+                [contactList addContact:newContact];
+                
             }
             else if ([mainMenu isEqualToString:@"list"]) {
-                NSLog(@"LIST");
+                for (int i = 0; i < contactList.contacts.count; i++) {
+                    NSLog(@"");
+                }
             }
             else if ([mainMenu isEqualToString:@"quit"]) {
                 gameOn = NO;
